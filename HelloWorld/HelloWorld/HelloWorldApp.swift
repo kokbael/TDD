96e609d1b8e240a6447aebcf5901f420ef19e9b3
kdy <kokbael@naver.com>
2025-04-14 14:25:35 +0900

    XCTest 코드 작성 ( UITest 코드, 타겟 삭제 )

---
+++ HelloWorld/HelloWorld/HelloWorldApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloWorldApp.swift
+//  HelloWorld
+//
+//  Created by 김동영 on 4/14/25.
+//
+
+import SwiftUI
+
+@main
+struct HelloWorldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
