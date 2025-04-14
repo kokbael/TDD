2d67f5e2c47932a56b3f36605be190d5e641ba7a
kdy <kokbael@naver.com>
2025-04-14 15:04:35 +0900

    FizzBuzz 테스트 케이스 추가

---
+++ HelloWorld/FizzBuzzTests/FizzBuzzTests.swift
@@ -0,0 +1,41 @@
+//
+//  FizzBuzzTests.swift
+//  FizzBuzzTests
+//
+//  Created by 김동영 on 4/14/25.
+//
+
+import XCTest
+
+final class FizzBuzzTests: XCTestCase {
+    // 제일 처음에 호출
+    override func setUpWithError() throws {
+        print("setUpWithError")
+    }
+    
+    // 제일 마지막에 호출
+    override func tearDownWithError() throws {
+        print("tearDownWithError")
+    }
+    
+    func testFizzBuzzDivisibleBy3() throws {
+        let result = fizzBuzz(3)
+        XCTAssertEqual(result, "fizz")
+    }
+    
+    func testFizzBuzzDivisibleBy5() throws {
+        let result = fizzBuzz(5)
+        XCTAssertEqual(result, "buzz")
+    }
+    
+    func testFizzBuzzDivisibleBy15() throws {
+        let result = fizzBuzz(15)
+        XCTAssertEqual(result, "fizz-buzz")
+    }
+    
+    func testFizzBuzzNotDivisibaleBy3Or5ReturnInput() throws {
+        let result = fizzBuzz(7)
+        XCTAssertEqual(result, "7")
+
+    }
+}
---
+++ HelloWorld/HelloWorld/FizzBuzz.swift
@@ -0,0 +1,10 @@
+//
+//  FizzBuzz.swift
+//  HelloWorld
+//
+//  Created by 김동영 on 4/14/25.
+//
+
+public func fizzBuzz(_ number: Int) -> String{
+    return ""
+}
