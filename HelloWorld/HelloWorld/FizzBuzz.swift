4393908da5ed2ba589b015c33429bd3487a62ec4
kdy <kokbael@naver.com>
2025-04-14 15:17:40 +0900

    FizzBuzz 함수 구현 및 반환값 수정

--- HelloWorld/HelloWorld/FizzBuzz.swift
+++ HelloWorld/HelloWorld/FizzBuzz.swift
@@ -5,6 +5,14 @@
 //  Created by 김동영 on 4/14/25.
 //
 
-public func fizzBuzz(_ number: Int) -> String{
-    return ""
+func fizzBuzz(_ number: Int) -> String {
+    if number % 3 == 0 && number % 5 == 0 {
+        return "fizz-buzz"
+    } else if number % 3 == 0 {
+        return "fizz"
+    } else if number % 5 == 0 {
+        return "buzz"
+    } else {
+        return "\(number)"
+    }
 }
