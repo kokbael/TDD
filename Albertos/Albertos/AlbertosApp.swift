f201d457ad30f663a308000b04e8fe15ac6b5915
kdy <kokbael@naver.com>
2025-04-15 11:29:41 +0900

    새 프로젝트 생성, AlbertosUITests 및 AlbertosUITestsLaunchTests 파일 삭제

---
+++ Albertos/Albertos/AlbertosApp.swift
@@ -0,0 +1,17 @@
+//
+//  AlbertosApp.swift
+//  Albertos
+//
+//  Created by 김동영 on 4/15/25.
+//
+
+import SwiftUI
+
+@main
+struct AlbertosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
