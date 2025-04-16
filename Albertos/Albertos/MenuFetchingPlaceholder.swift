85450040fb69d10ea03a1ec5e97fd370605915b3
kdy <kokbael@naver.com>
2025-04-16 14:40:37 +0900

    MenuFetchingPlaceholder에서 메뉴 가져오기 로직을 랜덤 실패 및 성공으로 수정하고, MenuList 뷰에서 오류 발생 시 재시도 버튼 추가

--- Albertos/Albertos/MenuFetchingPlaceholder.swift
+++ Albertos/Albertos/MenuFetchingPlaceholder.swift
@@ -10,8 +10,16 @@ import Combine
 
 class MenuFetchingPlaceholder: MenuFetching {
     func fetchMenu() -> AnyPublisher<[MenuItem], Error> {
-        return Future { $0(.success(menu)) }
-            .delay(for: 0.5, scheduler: RunLoop.main)
-            .eraseToAnyPublisher()
+        return Future {
+            // 랜덤하게 실패와 성공을 발행
+            let isSuccess = Bool.random()
+            if isSuccess {
+                $0(.success(menu))
+            } else {
+                $0(.failure(NSError(domain: "API 호출에 실패 했습니다.", code: -1, userInfo: nil)))
+            }
+        }
+        .delay(for: 0.5, scheduler: RunLoop.main)
+        .eraseToAnyPublisher()
     }
 }
--- Albertos/Albertos/MenuList.swift
+++ Albertos/Albertos/MenuList.swift
@@ -25,8 +25,19 @@ struct MenuList: View {
             case .failure(let error):
                 Text("An error occurred:")
                 Text(error.localizedDescription).italic()
+                Button("Retry") {
+                    viewModel.retry()
+                }
+                .padding()
+                .background(Color.blue)
+                .foregroundStyle(.white)
+                .clipShape(RoundedRectangle(cornerRadius: 8))
             }
         }
+        .refreshable {
+            viewModel.retry()
+        }
+        
         .navigationTitle("Alberto's 🇮🇹")
     }
 }
@@ -35,27 +46,41 @@ extension MenuList {
     class ViewModel: ObservableObject {
         @Published private(set) var sections: Result<[MenuSection], Error> = .success([])
         
+        private let menuFetching: MenuFetching
+        private let menuGrouping: ([MenuItem]) -> [MenuSection]
+        
         var cancellables = Set<AnyCancellable>()
         
         init(
             menuFetching: MenuFetching,
             menuGrouping: @escaping ([MenuItem]) -> [MenuSection] = groupMenuByCategory) {
-                menuFetching
-                    .fetchMenu()
-                // [중요] 전달 받은 [MenuItem] 시퀀스를 menuGrouping 함수에 전달하도록 수정
-                    .map(menuGrouping)
-                // [중요] Result<[MenuSection], Error> 타입으로 변환
-                    .sink(
-                        receiveCompletion: { [weak self] completion in
-                            if case .failure(let error) = completion {
-                                self?.sections = .failure(error)
-                            }
-                        },
-                        receiveValue: { [weak self] value in
-                            self?.sections = .success(value)
-                        })
-                    .store(in: &cancellables)
+                self.menuFetching = menuFetching
+                self.menuGrouping = menuGrouping
+                fetchMenu()
             }
+        
+        func fetchMenu() {
+            menuFetching
+                .fetchMenu()
+            // [중요] 전달 받은 [MenuItem] 시퀀스를 menuGrouping 함수에 전달하도록 수정
+                .map(menuGrouping)
+            // [중요] Result<[MenuSection], Error> 타입으로 변환
+                .sink(
+                    receiveCompletion: { [weak self] completion in
+                        if case .failure(let error) = completion {
+                            self?.sections = .failure(error)
+                        }
+                    },
+                    receiveValue: { [weak self] value in
+                        self?.sections = .success(value)
+                    })
+                .store(in: &cancellables)
+        }
+        
+        func retry() {
+            fetchMenu()
+        }
+        
     }
 }
 
