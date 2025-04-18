0d3a3063f8414efd5825be6b47a1bea2fcea448f
kdy <kokbael@naver.com>
2025-04-18 10:31:41 +0900

    주문 상세 화면의 UI 구현 및 상태 관리 로직 추가

--- Albertos/Albertos/Views/OrderDetail.swift
+++ Albertos/Albertos/Views/OrderDetail.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import Combine
 
 struct OrderDetail: View {
     @StateObject private var viewModel: ViewModel
@@ -16,8 +17,20 @@ struct OrderDetail: View {
     
     var body: some View {
         List {
-            // TODO: 주문 목록을 보여주는 UI
-        }
+            ForEach(viewModel.orderItems) { item in
+                HStack {
+                    Text(item.name)
+                        .font(.headline)
+                    Spacer()
+                    Text("$\(item.price, specifier: "%.2f")")
+                        .foregroundColor(.gray)
+                }
+            }
+            
+            Section(header: Text("총 가격")) {
+                Text("$\(viewModel.totalPrice, specifier: "%.2f")")
+                    .font(.headline)
+            }        }
         .navigationTitle("주문 상세")
     }
 }
@@ -26,8 +39,25 @@ extension OrderDetail {
     class ViewModel: ObservableObject {
         let orderController: OrderController
         
+        @Published private(set) var orderItems: [MenuItem] = []
+        @Published private(set) var totalPrice: Double = 0.0
+        
+        private var cancellables = Set<AnyCancellable>()
+        
         init(orderController: OrderController) {
             self.orderController = orderController
+            
+            setupSubscriptions()
+        }
+        
+        private func setupSubscriptions() {
+            orderController.$order
+                .sink { [weak self] order in
+                    guard let self = self else { return }
+                    self.orderItems = order.items
+                    self.totalPrice = order.items.reduce(0) { $0 + $1.price }
+                }
+                .store(in: &cancellables)
         }
     }
 }
